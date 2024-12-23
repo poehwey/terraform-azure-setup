c87a6c7bfa76bad04efb029dd1af1a13257d3e43
Brandon Quetstroey <brandon.quetstroey@student.howest.be>
2024-12-23 15:54:17 +0100

    Update Terraform provider and required version for compatibility

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.7.0"
+      version = "~> 3.0"
     }
   }
-  required_version = "=4.7.0"
+  required_version = ">= 1.5.0"
 
   backend "azurerm" {
     resource_group_name  = "quetstroey-tfstate"
